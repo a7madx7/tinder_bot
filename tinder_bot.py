c08080b4000984e689cc1675e7f17b1d14f84b06
Ahmad Hamdi <a7madx7@users.noreply.github.com>
2020-05-08 02:51:50 +0300

    Better at login, more robust and agile, funnier.
    Re-wrote the whole script with best practices in mind.

--- tinder_bot.py
+++ tinder_bot.py
@@ -10,6 +10,8 @@ Please run with python3 tinder_bot.py not with python tinder_bot.py.
 # Imports region.
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import random
 
 """
 Create a secrets file right next to tinder_bot and put two string variables
@@ -27,8 +29,8 @@ from time import sleep
 class Config:
     # Number Configs
     swipe_wait: float = 0.5
-    short_wait: int = 1
-    long_wait: int = 4
+    short_wait: float = 1
+    long_wait: float = 2
     soft_swipe_limit: int = 2000
 
     # Buttons XPathes
@@ -37,6 +39,7 @@ class Config:
     popup2_btn_xpath: str = '//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]'
 
     popup3_btn_xpath: str = '//*[@id="modal-manager"]/div/div/div[2]/button[2]'
+
     match_popup_xpath: str = '//*[@id="modal-manager-canvas"]/div/div/div[1]/div/div[3]/a'
     
     more_options_btn_xpath: str = '//*[@id="modal-manager"]/div/div/div/div/div[3]/span/button'
@@ -44,15 +47,20 @@ class Config:
     dislike_btn_xpath: str = '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[2]/button'
     accept_cookies_btn_xpath: str = '//*[@id="content"]/div/div[2]/div/div/div[1]/button'
     fb_btn_xpath: str = '//*[@id="modal-manager"]/div/div/div/div/div[3]/span/div[2]/button'
-    
+
     # Inputs
     email_input_xpath: str = '//*[@id="email"]'
     pw_input_xpath: str = '//*[@id="pass"]'
 
     # URLs
     tinder_url: str = 'https://tinder.com'
-    
 
+class AI():
+    def praise_the_bitch(self):
+        praises = ['Awe that girl was hot dude', 'I guess ur gonna get what u see my man', 
+        "Nigga this bitch ain't for hookups", "That curvy babe <3", "Will you marry me?",
+        "That's a hell of an ass my dude!", "Saw dat bitch? daaaaamn", "Fuck my life", "How cute!"]
+        print(random.choice(praises))
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 # Bot class region.
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -60,116 +68,124 @@ class TinderBot():
     def __init__(self):
         self.driver = webdriver.Chrome()
         self.config = Config()
+        self.ai = AI()
 
-    def accept_cookies(self):
+    def _accept_cookies(self):
         try:
-            accept_cookies_btn = self.driver.find_element_by_xpath(self.config.accept_cookies_btn_xpath)
-            accept_cookies_btn.click()
+            self._click_a_button(self.config.accept_cookies_btn_xpath)
             return self
         except:
             return self
 
-    def login(self):
-        self.driver.get(self.config.tinder_url)
+    def _fill_credentials(self):
+        self.driver.find_element_by_xpath(self.config.email_input_xpath).send_keys(username)
+        self.driver.find_element_by_xpath(self.config.pw_input_xpath).send_keys(password)
+        self._click_a_button(self.config.login_btn_xpath)
+        print("Done filling credentials")
 
-        sleep(self.config.long_wait)
-
-        self.accept_cookies()
-
-        # pressing more options button
-        pressing_more_options = True
-        while(pressing_more_options):
+    def _like(self):
+        self._click_a_button(self.config.like_btn_xpath)
+        
+    def _dislike(self):
+        self._click_a_button(self.config.dislike_btn_xpath)
+    def _swipe_right(self):
+        sleep(self.config.swipe_wait)
+        try:
+            self._like()
+            self.ai.praise_the_bitch()
+            return True
+        except:
             try:
-                more_options_btn = self.driver.find_element_by_xpath(self.config.more_options_btn_xpath)
-                more_options_btn.click()
-                pressing_more_options = False
-            except Exception as e:
-                print(e)
-                print("Trying to press more options one more time...")
+                self._click_a_button(self.config.popup3_btn_xpath)
+                return False
+            except:
+                try:
+                    self._close_match()
+                    return False
+                except:
+                    return False
+
+    def _click_a_button(self, xpath):
+        self.driver.find_element_by_xpath(xpath).click()
+
+    def _close_all_popups(self):
+        try:
+            self._click_a_button(self.config.popup1_btn_xpath)
+        except:
+            try:
+                self._click_a_button(self.config.popup2_btn_xpath)
+            except:
+                try:
+                    self._click_a_button(self.config.popup3_btn_xpath)
+                except:
+                    raise Exception("It's about time you review your bot for tinder changes!")
+                
+    def _close_match(self):
+        self._click_a_button(self.config.match_popup_xpath)
+
+    def _persist_pressing_a_button(self, xpath):
+        pressing = True
+        while(pressing):
+            try:
+                self._click_a_button(xpath)
+                pressing = False
+            except NoSuchElementException as ex:
                 sleep(self.config.short_wait)
                 continue
 
-        # pressing fb login button
-        logging_in = True
-        while(logging_in):
-            try:
-                fb_btn = self.driver.find_element_by_xpath(self.config.fb_btn_xpath)
-                fb_btn.click()
-                logging_in = False
-            except Exception as e:
-                print(e)
-                print("Trying to press fb login one more time...")
-                sleep(self.config.short_wait)
-                continue
-
-        # switch to login popup
+    def _switch_to_login(self):
+        # switch to login window
         switching_to_login = True
         while(switching_to_login):
             try:
                 base_window = self.driver.window_handles[0]
                 self.driver.switch_to.window(self.driver.window_handles[1])
                 switching_to_login = False
+                self._fill_credentials()
+                self.driver.switch_to.window(base_window)
+                return True
             except IndexError:
                 print("Trying to switch windows one more time...")
                 sleep(self.config.short_wait)
                 continue
 
-        email_in = self.driver.find_element_by_xpath(self.config.email_input_xpath)
-        email_in.send_keys(username)
+    def _element_exists(self, xpath):
+        """
+        Used mainly to check for Tinder antibot procedures.
+        """
+        try:
+            self.driver.find_element_by_xpath(xpath)
+            return True
+        except NoSuchElementException as e:
+            print("Tinder is trying to prevent us, that won't help it a lot though...")
+            return False
 
-        pw_in = self.driver.find_element_by_xpath(self.config.pw_input_xpath)
-        pw_in.send_keys(password)
+    def _attempt_login(self):
+        if self._element_exists(self.config.fb_btn_xpath):
+            self._persist_pressing_a_button(self.config.fb_btn_xpath)
+        elif self._element_exists(self.config.login_btn_xpath):
+            self._persist_pressing_a_button(self.config.login_btn_xpath)
+            sleep(self.config.swipe_wait)
+            self._persist_pressing_a_button(self.config.fb_btn_xpath)
+            self._persist_pressing_a_button(self.config.more_options_btn_xpath)
+        elif self._element_exists(self.config.more_options_btn_path):
+            self._persist_pressing_a_button(self.config.more_options_btn_xpath)
+            self._persist_pressing_a_button(self.config.fb_btn_xpath)
+    def login(self):
+        self.driver.get(self.config.tinder_url)
+        print("TinderBot is starting, please wait...")
+        sleep(self.config.long_wait)
 
-        login_btn = self.driver.find_element_by_xpath(self.config.login_btn_xpath)
-        login_btn.click()
+        self._accept_cookies()
+        self._attempt_login()
 
-        self.driver.switch_to.window(base_window)
-        sleep(self.config.short_wait)
-        closing_popup_1 = True
-        while(closing_popup_1):
-            try:
-                popup_1 = self.driver.find_element_by_xpath(self.config.popup1_btn_xpath)
-                popup_1.click()
-                closing_popup_1 = False
-            except:
-                sleep(self.config.short_wait)
-                print('Trying to close Popup 1...')
-                continue
+        self._switch_to_login()
 
-        closing_popup_2 = True
-        while(closing_popup_2):
-            try:
-                popup_2 = self.driver.find_element_by_xpath(self.config.popup2_btn_xpath)
-                popup_2.click()
-                closing_popup_2 = False
-            except:
-                break
-
+        self._persist_pressing_a_button(self.config.popup1_btn_xpath)
+        self._persist_pressing_a_button(self.config.popup2_btn_xpath)
         return self
 
-    def like(self):
-        like_btn = self.driver.find_element_by_xpath(self.config.like_btn_xpath)
-        like_btn.click()
-
-    def dislike(self):
-        dislike_btn = self.driver.find_element_by_xpath(self.config.dislike_btn_xpath)
-        dislike_btn.click()
-
-    def _swipe_right(self):
-        sleep(self.config.swipe_wait)
-        try:
-            self.like()
-            return True
-        except:
-            try:
-                self.close_popup()
-                return False
-            except:
-                try:
-                    self.close_match()
-                    return False
-                except:
-                    return False
+    
 
     def auto_swipe(self):
         print('Initiating auto-swipe, it will take around 20 minutes to get the job done.')
@@ -178,15 +194,6 @@ class TinderBot():
             if self._swipe_right():
                 counter += 1
         print(f'Done auto-swiping {self.config.soft_swipe_limit} babes!!')
-
-    def close_popup(self):
-        popup_3 = self.driver.find_element_by_xpath(self.config.popup3_btn_xpath)
-        popup_3.click()
-
-    def close_match(self):
-        match_popup = self.driver.find_element_by_xpath(self.config.match_popup_xpath)
-        match_popup.click()
-
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 # Automation start region.
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
